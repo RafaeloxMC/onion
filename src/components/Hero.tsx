19b5c4357ccb013411de2020e2c62848b65e24af
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2026-08-11 10:30:17 -0700

    Add catch phrase to Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,10 +2,14 @@ import Accent from "./Accent";
 
 function Hero() {
 	return (
-		<div>
-			<h1 className="flex flex-row items-center justify-center gap-4 h-32">
+		<div className="flex flex-col items-center gap-6 my-8">
+			<h1 className="flex flex-row gap-4">
 				Welcome to <Accent>Onion Ysws</Accent>
 			</h1>
+			<span className="text-xs italic opacity-50">
+				Ship a project that makes every-day privacy easier, get cool
+				prizes!
+			</span>
 		</div>
 	);
 }
